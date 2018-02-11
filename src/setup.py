8edc1bbff28d37402f5ebecf2d2839cbed54976f
Shiro Ryougi <avalan4er@gmail.com>
2018-02-11 22:00:47 +0300

    Created setup

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='HB',
+    version='1.0.0.0',
+    packages=['', 'bot', 'framework', 'configuration', '', '', ''],
+    package_dir={'': 'src'},
+    url='https://github.com/Avalan4er/HB',
+    license='MIT',
+    author='avalan4er',
+    author_email='avalan4er@gmail.com',
+    description='Heroes Of The Storm education project'
+)
