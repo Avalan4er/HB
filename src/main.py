b02c080a7a71c27c0bab067a721a47aa8f92e680
Олег Дорофеев <o.dorofeev@infocom-s.ru>
2018-02-12 12:13:44 +0300

    Build exe with pyinstaller -F main.spec

--- src/main.py
+++ src/main.py
@@ -1,37 +1,45 @@
 import logging
 import state_machine
 import random
+import sys
+
+
+def setup_environment():
+    sys.path.insert(0, '/bot')
+    sys.path.insert(0, '/configuration')
+    sys.path.insert(0, '/framework')
 
 
 def main():
     application = state_machine.Application()
     application.run()
 
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S')
     # logging.disable(logging.DEBUG) # uncomment to block debug log messages
     random.seed(12331551)
+    setup_environment()
     main()
 
+    # import state_machine
+    # from objects import Map, MapStop
 
-    #import state_machine
-    #from objects import Map, MapStop
+    # map = Map('TowersOfDoom', [MapStop(1641, 912), MapStop(1751, 912)], 'towers_of_doom')
+    # player = state_machine.Player('left_side', map)
+    # player.wait_for_game_start()
 
-    #map = Map('TowersOfDoom', [MapStop(1641, 912), MapStop(1751, 912)], 'towers_of_doom')
-    #player = state_machine.Player('left_side', map)
-    #player.wait_for_game_start()
+    # from PIL import Image
+    # import hots
+    # from objects import Map, MapStop
+    # import vision_helpers
 
-
-
-    #from PIL import Image
-    #import hots
-    #from objects import Map, MapStop
-    #import vision_helpers
-
-    #current_map =  Map('GardenOfTerror',
+    # current_map =  Map('GardenOfTerror',
     #    [MapStop(1552, 909), MapStop(1584, 904), MapStop(1641, 909), MapStop(1751, 899), MapStop(1807, 905), MapStop(1839, 896)],
     #   'garden_of_terror')
 
-    #screenshot = Image.open('creeps.png')
-    #coords = list(vision_helpers.find_all_enemy_creeps(screenshot))
-    #print('t')
+    # screenshot = Image.open('creeps.png')
+    # coords = list(vision_helpers.find_all_enemy_creeps(screenshot))
+    # print('t')
+
+
---
+++ src/main.spec
@@ -0,0 +1,52 @@
+# -*- mode: python -*-
+
+import os
+
+block_cipher = None
+
+#  PROJECT_PATH = os.path.dirname(os.path.realpath(os.__file__))
+PROJECT_PATH = os.getcwd()
+
+a = Analysis([
+                'main.py',
+                'bot\\hots.py',
+                'bot\\state_machine.py',
+                'configuration\\config.py',
+                'configuration\\constants.py',
+                'configuration\\objects.py',
+                'framework\\screenshot_helpers.py',
+                'framework\\vision_helpers.py',
+                'framework\\windows_helpers.py'
+            ],
+             pathex=[
+                 PROJECT_PATH,
+                 os.path.join(PROJECT_PATH, 'bot'),
+                 os.path.join(PROJECT_PATH, 'configuration'),
+                 os.path.join(PROJECT_PATH, 'framework'),
+             ],
+             binaries=[],
+             datas=[
+                ( os.path.join(PROJECT_PATH, 'resources\\img\\*.png'), 'src\\resources\\img'),
+                ( os.path.join(PROJECT_PATH, 'resources\\img\\loading_screen\\*.png'), 'src\\resources\\img\\loading_screen')
+             ],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          name='HOTS Bot',
+          debug=False,
+          strip=False,
+          upx=True,
+          runtime_tmpdir=None,
+          console=True,
+          icon='resources\\img\\icon.ico')
